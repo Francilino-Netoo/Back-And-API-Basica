564c99ca8e84a594cb9931f6bc79cb86bb713e41
Francilino-Netoo <francilino12345@gmail.com>
2025-03-13 23:22:52 -0300

    ajustes

--- server.js
+++ server.js
@@ -3,21 +3,19 @@ const cors = require("cors");
 const fileupload = require("express-fileupload");
 const mongoose = require("mongoose");
 const express = require("express");
+const path = require("path");
 
 const apiRoutes = require("./src/routes");
 
-// Conectar ao MongoDB com opções adicionais
+// Conectar ao MongoDB
 mongoose
   .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     retryWrites: true,
     w: "majority",
   })
   .then(() => console.log("✅ Conectado ao MongoDB"))
   .catch((error) => console.error("❌ Erro ao conectar ao MongoDB:", error));
 
-mongoose.Promise = global.Promise;
 mongoose.connection.on("error", (error) => {
   console.log("❌ Erro de conexão:", error.message);
 });
@@ -28,14 +26,14 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(fileupload());
 
-server.use(express.static(__dirname + "/public"));
+server.use(express.static(path.join(__dirname, "public")));
 server.use("/", apiRoutes);
 
 server.get("/ping", (req, res) => {
   res.json({ status: "API Online", port: process.env.PORT });
 });
 
-// Verifica se a porta está definida antes de iniciar o servidor
+// Inicia o servidor
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(
